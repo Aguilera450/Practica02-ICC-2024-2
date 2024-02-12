4476cfb80ea2c36ea08b59605aa6b44bf7f27ca8
Aguilera450 <aguilera@ciencias.unam.mx>
2024-02-11 20:14:20 -0600

    Initial commit.

---
+++ Init/Problema02.java
@@ -0,0 +1,36 @@
+/**
+ * Archivo a modificar.
+ * Modifica el código ASCII que representa a un oso y elimina una parte de su bufanda.
+ * @author Adrián Aguilera      (modificar a su respectivo nombre y agrega tus datos).
+ * @version 1.0                 (modifica el número de versión).
+ * @date 12 feb 2024.           (modifica la fecha a la fecha de elaboración).
+ */
+
+public class Problema02{
+    public static void main(String[] pps){
+	String oso = "\n\n" +
+	    " .-\"\"-.          ( )-\"```\"-( )          .-\"\"-.\n" +
+	    "/ O O  \\          /         \\          /  O O \\\n" +
+	    "|O .-.  \\        /   0 _ 0   \\        /  .-. O|\n" +
+	    "\\ (   )  '.    _|     (_)     |     .'  (   ) /\n" +
+	    " '.`-'     '-./ |             |`\\.-'     '-'.\\'\n" +
+	    "   \\         |  \\   \\     /   /  |         /\n" +
+	    "    \\        \\   '.  '._.'  .'   /        /\n" +
+	    "     \\        '.   `'-----'`   .'        /\n" +
+	    "      \\   .'    '-._        .-'\\   '.   /\n" +
+	    "       |/`          `'''''')    )    `\\|\n" +
+	    "        /                  (    (      ,\\\n" +
+	    "       ;                    \\    '-..-'/ ;\n" +
+	    "       |                     '.       /  |\n" +
+	    "       |                       `'---'`   |\n" +
+	    "       ;                                 ;\n" +
+	    "        \\                               /\n" +
+	    "         `.                           .'\n" +
+	    "           '-._                   _.-'\n" +
+	    "            __/`\"  '  - - -  ' \"`` \\__\n" +
+	    "          /`            /^\\           `\\\n" +
+	    "          \\(          .'   '.         )/\n" +
+	    "           '.(__(__.-'       '.__)__).'";
+	System.out.println(oso);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Practica02-ICC-2024-2
+# Introducción a las Ciencias de la Computación
+## Práctica02
+Al finalizar esta práctica, el alumno habrá reforzado sus conocimientos acerca de la creación
+y manipulación de objetos en Java de la clase String.
+
+## Alumno
+(Nombre) (ApellidoPaterno) (ApellidoMaterno)
+
+(No. Cuenta)
+
+(Correo)
+
+## Instrucciones
+(Escribe las instrucciones de ejecución aquí)
+
+## Comentarios
+1. No se pueden utilizar instrucciones de control. Es importante que comentes tú código.
+2. Se debe usar el objeto String y sus respectivos métodos asociados.
+3. La fecha de entrega se acordará en el laboratorio de clases.
+4. La evaluación se encuentra el la rúbrica del classroom.
+5. Al finalizar se debe compartir el link HTTPS y SSH por el classroom.
